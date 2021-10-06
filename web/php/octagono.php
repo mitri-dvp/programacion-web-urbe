beae3a31d517591b7e7b39d2b07bcb5686558de4
mitri.dvp <mitri.dvp@gmail.com>
2021-10-06 12:13:16 -0500

    Ocatagon setup

---
+++ web/php/octagono.php
@@ -0,0 +1,18 @@
+<?php
+  echo '<head>';
+  echo '<meta charset="UTF-8">';
+  echo '<title>Cálculo de la Hipotenusa del Triángulo</title>';
+  echo '<link rel="stylesheet" href="../css/style.css">';
+  echo '<link rel="icon" href="../assets/icon.svg">';
+  echo '</head>';
+
+  echo '<div id="octagono">';
+  echo '<h3>Calcular el área de un octágono regular:</h3>';
+
+  echo '<div class="links">';
+  echo '<a href="../index.html">Volver</a>';
+  echo '<a target="_blank"href="https://github.com/mitri-dvp/programacion-web-urbe/blob/main/web/php/hipotenusa.php">Repositorio</a>';
+  echo '</div>';
+  
+  echo '</div>';
+?>
