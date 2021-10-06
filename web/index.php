5cfd23b7beccb745535f9d138e510b42eb49745a
mitri.dvp <mitri.dvp@gmail.com>
2021-10-05 23:30:31 -0500

    HTML added.

--- web/index.php
+++ web/index.php
@@ -1,3 +1,13 @@
 <?php
-  echo 'Hello World';
+  $lado_a = 3;
+  $lado_b = 4;
+
+  $hipotenusa = sqrt(pow($lado_a, 2) + pow($lado_b, 2));
+  
+  echo '<h3>Calculo de la Hipotenusa del Triangulo</h3>';
+  echo '<p>Lado A del Triangulo: ' . $lado_a, ' cm</p>';
+  echo '<p>Lado B del Triangulo: ' . $lado_b, ' cm</p>';
+  echo '<p>Hipotenusa del Triangulo: ' . $hipotenusa, ' cm</p>';
+
+  echo '<a href="/index.html">Volver</a>';
 ?>
