a62eecfb050156fa36a72827e5faeeb9a878e18e
mitri.dvp <mitri.dvp@gmail.com>
2021-10-13 13:01:36 -0500

    Ocatagon PHP activity finished

--- web/php/hipotenusa.php
+++ web/php/hipotenusa.php
@@ -1,27 +1,35 @@
 <?php
-  echo '<head>';
-  echo '<meta charset="UTF-8">';
-  echo '<title>Cálculo de la Hipotenusa del Triángulo</title>';
-  echo '<link rel="stylesheet" href="../css/style.css">';
-  echo '<link rel="icon" href="../assets/icon.svg">';
-  echo '</head>';
+$lado_a = 3;
+$lado_b = 4;
 
-  echo '<div id="hipotenusa">';
-  echo '<h3>Cálculo de la Hipotenusa del Triángulo:</h3>';
-
-  $lado_a = 3;
-  $lado_b = 4;
-  
-  $hipotenusa = sqrt(pow($lado_a, 2) + pow($lado_b, 2));
-
-  echo '<p>Lado A del Triangulo: <span>' . $lado_a, ' cm</span></p>';
-  echo '<p>Lado B del Triangulo: <span>' . $lado_b, ' cm</span></p>';
-  echo '<p>Hipotenusa del Triangulo: <span>' . $hipotenusa, ' cm</span></p>';
-
-  echo '<div class="links">';
-  echo '<a href="../index.html">Volver</a>';
-  echo '<a target="_blank"href="https://github.com/mitri-dvp/programacion-web-urbe/blob/main/web/php/hipotenusa.php">Repositorio</a>';
-  echo '</div>';
-  
-  echo '</div>';
+$hipotenusa = sqrt(pow($lado_a, 2) + pow($lado_b, 2));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Cálculo de la Hipotenusa del Triángulo</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="icon" href="../assets/icon.svg">
+</head>
+
+<body>
+  <div id="hipotenusa">
+    <h3 class="title">Cálculo de la Hipotenusa del Triángulo:</h3>
+
+    <div class="results">
+      <p>Lado A del Triangulo: <span><?= $lado_a ?> cm</span></p>
+      <p>Lado B del Triangulo: <span><?= $lado_b ?> cm</span></p>
+      <p>Hipotenusa del Triangulo: <span><?= $hipotenusa ?> cm</span></p>
+    </div>
+
+    <div class="links">
+      <a href="../index.html">Volver</a>
+      <a target="_blank" href="https://github.com/mitri-dvp/programacion-web-urbe/blob/main/web/php/hipotenusa.php">Repositorio</a>
+    </div>
+  </div>
+</body>
+
+</html>
--- web/php/octagono.php
+++ web/php/octagono.php
@@ -1,18 +1,54 @@
 <?php
-  echo '<head>';
-  echo '<meta charset="UTF-8">';
-  echo '<title>Cálculo de la Hipotenusa del Triángulo</title>';
-  echo '<link rel="stylesheet" href="../css/style.css">';
-  echo '<link rel="icon" href="../assets/icon.svg">';
-  echo '</head>';
-
-  echo '<div id="octagono">';
-  echo '<h3>Calcular el área de un octágono regular:</h3>';
-
-  echo '<div class="links">';
-  echo '<a href="../index.html">Volver</a>';
-  echo '<a target="_blank"href="https://github.com/mitri-dvp/programacion-web-urbe/blob/main/web/php/hipotenusa.php">Repositorio</a>';
-  echo '</div>';
-  
-  echo '</div>';
+if (isset($_POST['longitud_lado'])) {
+  $longitud_lado = $_POST['longitud_lado'];
+} else {
+  $longitud_lado = null;
+}
+$lados = 8;
+$angulo_central = 360 / $lados;
+$angulo_central_rad = $angulo_central * pi() / 180;
+$apotema =  $longitud_lado / (2 * tan($angulo_central_rad / 2));
+$area = 4 * $longitud_lado * $apotema;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Calcular el área de un octágono regular</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="icon" href="../assets/icon.svg">
+</head>
+
+<body>
+  <div id="octagono">
+    <h3 class="title">Cálculo del Área del Octágono Regular:</h3>
+
+    <?php if ($longitud_lado !== null) { ?>
+      <div class="results">
+        <p>Longitud del Lado del Octágono: <span><?= $longitud_lado ?> cm</span></p>
+        <p>Ángulo Central del Octágono: <span><?= $angulo_central ?>&deg;</span></p>
+        <p>Apotema del Octágono: <span><?= number_format($apotema, 2) ?> cm</span></p>
+        <p>Área del Octágono: <span><?= number_format($area, 2) ?> cm&sup2;</span></p>
+      </div>
+    <?php } ?>
+
+    <hr>
+
+    <form method="post" action="">
+      <label>Introduzca la longitud del lado:</label>
+      <input type="number" min="0" name="longitud_lado" required>
+      <button type="submit">Enviar</button>
+    </form>
+
+    <hr>
+
+    <div class=" links">
+      <a href="../index.html">Volver</a>
+      <a target="_blank" href="https://github.com/mitri-dvp/programacion-web-urbe/blob/main/web/php/octagono.php">Repositorio</a>
+    </div>
+  </div>
+</body>
+
+</html>
