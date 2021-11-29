c4ca3ca6c6dae42ddee534eb6dd6a486b013bf16
mitri-dvp <mitri.dvp@gmail.com>
2021-11-28 21:52:35 -0500

    Oil wells setup.

---
+++ web/php/oil_wells/index.php
@@ -0,0 +1,18 @@
+<?php
+// Parse URL
+$db_url = parse_url(getenv("DATABASE_URL"));
+$db_host = $db_url["host"];
+$db_user = $db_url["user"];
+$db_password = $db_url["pass"];
+$db_name = substr($db_url["path"], 1);
+
+// Connect to DB
+$conn_string = "host=".$db_host." port=5432 dbname=".$db_name." user=".$db_user." password=".$db_password;
+$conn = pg_connect($conn_string);
+
+ if($conn) {
+   echo 'Conected';
+ } else {
+   echo 'Not connected';
+ }
+?>
